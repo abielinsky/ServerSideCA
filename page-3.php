2391fa0865f903689e886be402184f5afda18c20
Abiel <d00251844@student.dkit.ie>
2023-02-13 15:27:34 +0000

    Starting of CA

---
+++ page-3.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Starter Template · Bootstrap v5.0</title>
+    <!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="mystyle.css" rel="stylesheet">
+  </head>
+  <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
+      <div class="container">
+        <a class="navbar-brand" href="index.html">Site Title</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" id="navbarsExample07">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          </ul>
+          <span class="d-flex">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="page-1.html">Page 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="page-3.html">Page 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="page-3.html">Page 3</a>
+              </li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </nav>
+
+<main class="container">
+  <div class="starter-template text-center">
+    <h1>Page 3</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</main><!-- /.container -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
