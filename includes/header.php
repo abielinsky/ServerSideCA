c0e20f4709baa9f8c1f5f0cb57aa7aa5c59d15c1
Abiel <d00251844@student.dkit.ie>
2023-02-14 13:44:15 +0000

    First Include

---
+++ includes/header.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Starter Template · Bootstrap v5.0</title>
--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!doctype html>
+
+
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.80.0">
-    <title>Starter Template · Bootstrap v5.0</title>
+
+<head>
+    
+<?php include 'includes/header.php'; ?>
+
     <!-- Bootstrap core CSS -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="mystyle.css" rel="stylesheet">
   </head>
+
+
+
   <body>
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Eighth navbar example">
