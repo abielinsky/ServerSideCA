ba973c916855044e4e995cd45513684b4c826355
Abiel <d00251844@student.dkit.ie>
2023-02-20 15:37:58 +0000

    form commit2

---
+++ contact-form-handler.php
@@ -0,0 +1,60 @@
+<?php
+
+$errors = '';
+$myemail = 'd00251844@student.dkit.ie';// <-----Put your DkIT email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['phone']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+// Important: Create email headers to avoid spam folder
+$headers .= 'From: '.$myemail."\r\n".
+    'Reply-To: '.$myemail."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+{
+        $to = $myemail;
+        $email_subject = "Contact form submission: $name";
+        $email_body = "You have received a new message. ".
+        " Here are the details:\n 
+        
+        Name: $name \n 
+        Email: $email_address \n 
+        phone: $phone \n 
+        Message \n $message";
+
+        mail($to,$email_subject,$email_body,$headers);
+        //redirect to the 'thank you' page
+        header('Location: thank-you.php');
+}
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+        <title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+</body>
+</html>
--- thank-you.php
+++ thank-you.php
@@ -51,10 +51,16 @@
     </nav>
 
 <main class="container">
-  
   <div class="starter-template text-center">
+
     <h1>Bootstrap starter template</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  
+
+    <h1>Thank you!</h1>
+     Thank you for submitting the form. We will contact you soon!
+
+  
   </div>
 
 </main><!-- /.container -->
