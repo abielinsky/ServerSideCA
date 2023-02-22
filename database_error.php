0a55c70a8512dd61e52cd40d3822cf862c96778e
Abiel <d00251844@student.dkit.ie>
2023-02-22 15:53:59 +0000

    update for the connection database

---
+++ database_error.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>Product Manager</title>
+ <!--   <link rel="stylesheet" type="text/css" href="main.css" />  -->
+</head>
+
+<!-- the body section -->
+<body>
+    <header><h1>Product Manager</h1></header>
+
+    <main>
+        <h1>Database Error</h1>
+        <p>There was an error connecting to the database.</p>
+        <p>The database must be installed as described in the appendix.</p>
+        <p>Error message: <?php echo $error_message; ?></p>
+        <p>&nbsp;</p>
+    </main>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Product Manager, Inc.</p>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,18 +1,81 @@
+<?php
+
+require_once('database.php');
+
+// Get products
+$queryProducts = 'SELECT * FROM products';
+$statement = $db->prepare($queryProducts);
+$statement->execute();
+$products = $statement->fetchAll();
+
+$statement->closeCursor();
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+        <head>
+            <?php include 'includes/header.php'; ?>
+        </head>
+
+        <body>
+        <header>
+            <h1>Product Manager</h1>
+            
+        </header>
+
+            <main class="container">
+                <div class="starter-template text-center">
+                    <h1>Bootstrap starter template</h1>
+                    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+                </div>
 
 
-<?php include 'includes/header.php'; ?>
-
-    <main class="container">
-        <div class="starter-template text-center">
-            <h1>Bootstrap starter template</h1>
-            <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-        </div>
 
 
 
+                <h1>Product List</h1>
+            <section>
+                <!-- display a table of products -->
+                <table>
+                    <tr>
+                        <th>Code</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Brand</th>
+                        <th>Discount</th>
+                        <th>Delete</th>
+                    
+                    </tr>
 
+                    <?php foreach ($products as $product) : ?> 
+                    <tr>
+                        <td><?php echo $product['productCode']; ?></td>
+                        <td><?php echo $product['productName']; ?></td>
+                    
+                        <td class="right"><?php echo $product['listPrice']; ?></td>
 
+                        <td><?php echo $product['brand']; ?></td>
 
-    </main><!-- /.container -->
+                        <td class="right"><?php echo $product['discount'] . '%'; ?></td>
+                    
+                        <td><form action="delete_product.php" method="post">
+                            <input type="hidden" name="product_id"
+                                value="<?php echo $product['productID']; ?>">
+                            <input type="submit" value="Delete">
+                        </form></td>
 
-<?php include 'includes/footer.php'; ?>
+                             
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    </section>
+
+            </main><!-- /.container -->
+
+            <footer>
+            <?php include 'includes/footer.php'; ?>
+            </footer>
+
+</html>
