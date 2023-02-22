661ce2282dd1df7acd6160d8e2a679908e2c9ace
Abiel <d00251844@student.dkit.ie>
2023-02-22 00:01:26 +0000

    working on the database
    working on the database

--- thank-you.php
+++ thank-you.php
@@ -5,8 +5,8 @@
 
       <div class="starter-template text-center">
 
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+<!--        <h1>Bootstrap starter template</h1>-->
+<!--        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>-->
 
 
         <h1 class="mt-10" > Thank you! </h1>
