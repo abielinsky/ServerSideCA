ce179e491eac2f8e978fea8f675af9774e0bfc4a
Abiel <d00251844@student.dkit.ie>
2023-03-06 15:07:02 +0000

    getting ready README.md

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# ServerSideCA
-Server Side Project
+# <a href="#" class="button">SERVER-SIDE CA</a>
+
+
+
+This is a sample project that demonstrates how to use PHP, HTML, 
+and SQL together to build a simple web application. 
+The application allows users to create and manage tasks using a 
+MySQL database.
+
+## <a href="#" class="button big">Getting Starter</a>
+
+To get started with this project, you'll need to do the following:
+
+1. Clone the repository to your local machine using `git clone`.
+2. Create a MySQL database and import the `database.sql` file to 
+create the necessary tables.
+3. Modify the `config.php` file with your database credentials.
+4. Start a local web server using a tool like XAMPP.
+
+Once you have everything set up, you should be able to 
+access the web application by visiting `http://localhost/` 
+in your web browser.
+
+## Features
+
+This project includes the following features:
+
+- Task management: Users can see information from the data 
+tables on the website.
+- Responsive design: The web application is designed to be 
+responsive and work well on mobile devices.
+
+## Technologies Used
+
+This project uses the following technologies:
+
+- PHP: Used to handle server-side logic and database interactions.
+- HTML/CSS: Used to create the user interface.
+- MySQL: Used to store data tables information.
+
+## Contributing
+
+If you'd like to contribute to this project, please feel 
+free to submit a pull request. 
+However, please note that this is a sample project 
+meant to demonstrate how to use PHP, HTML, and SQL together.
+
+## License
+
+This project is licensed under the MIT License. 
+See the LICENSE file for more information.
+
+## <a href="#" class="button">SERVER-SIDE CA</a>
+<input class="button" type="submit" value="Post comment (input)">
+<button class="button" type="submit">Post comment (button)</button>
--- contact.php
+++ contact.php
@@ -4,24 +4,24 @@
 <main class="container">
   <div class="starter-template text-center">
     <h1>CONTACT</h1>
-    <p class="lead">THE CONTACT FORM WILL BE HERE 
+    <p class="lead">THE CONTACT FORM WILL BE HERE
 
-        <form method="POST" name="contactform" action="contact-form-handler.php"> 
-        
+        <form method="POST" name="contactform" action="contact-form-handler.php">
+
         <p>
         <label for='name'>Your Name:</label> <br>
-        <input type="text" name="name">
+        <input type="text" name="name" required><br><br>
         </p>
 
         <p>
-        <label for="birthdate">Birthdate:</label> <br>
-			  <input type="date" id="birthdate" name="birthdate"><br><br>
+            <label for="birthdate">Birthdate:</label> <br>
+            <input type="date" id="birthdate" name="birthdate" required><br><br>
         </p>
 
         <p>
         <label for="gender">Gender:</label> <br>
-			<select id="gender" name="gender">
-				<option value="">--Please choose an option--</option>
+			<select id="gender" name="gender" required>
+				<option value="">-- Select Gender--</option>
 				<option value="male">Male</option>
 				<option value="female">Female</option>
 				<option value="other">Other</option>
@@ -29,70 +29,59 @@
         </p>
 
         <p>
-        <label for='email'>Email Address:</label> <br>
-        <input type="text" name="email"> <br>
+            <label for='email'>Email Address:</label> <br>
+            <input type="text" name="email" required><br><br>
         </p>
 
         <p>
-        <label for="phone">Enter a phone number:</label> <br>
-        <input type="tel" name="phone"
-        required><br><br>
+            <label for="phone">Enter a phone number:</label> <br>
+            <input type="tel" name="phone"
+            required><br><br>
         </p>
 
         <p>
-        <label for="country">Country:</label>   <br>
-			<input type="text" id="country" name="country" list="country-list">
-			<datalist id="country-list">
-				<option value="Ireland">
-				<option value="Canada">
-				<option value="Mexico">
-				<option value="Japan">
-				<option value="China">
-        <option value="Denmark">
-        <option value="Norway">
-        <option value="Iceland">
-        <option value="Peru">
-        <option value="Guatemala">
-        <option value="China">
-				<option value="Brazil">
+            <label for="country">Country:</label>   <br>
+                <input type="text" id="country" name="country" list="country-list">
+                <datalist id="country-list">
+                    <option value="Ireland">
+                    <option value="Canada">
+                    <option value="Mexico">
+                    <option value="Japan">
+                    <option value="China">
+                    <option value="Denmark">
+                    <option value="Norway">
+                    <option value="Iceland">
+                    <option value="Peru">
+                    <option value="Guatemala">
+                    <option value="China">
+                    <option value="Brazil" required>
 			</datalist><br><br>
         </p>
 
 
 
         <p>
-        <label for="terms">Agree to terms and conditions:</label>
-			  <input type="checkbox" id="terms" name="terms" required><br><br>
-        </p>
-
-
-
-
-
-
-        <p>
-        <label for='message'>Message:</label> <br>
-        <textarea name="message"></textarea>
+            <label for="terms">Agree to terms and conditions:</label>
+            <input type="checkbox" id="terms" name="terms" required><br><br>
         </p>
 
         <p>
-        <label for="datetime">Date and time:</label> <br>
-		    <input type="datetime-local" id="datetime" name="datetime"><br><br>
+            <label for='message'>Message:</label> <br>
+            <textarea name="message"></textarea> required><br><br>
         </p>
 
         <p>
-        <label for="file">Upload file:</label> <br>
-			  <input type="file" id="file" name="file"><br><br>
+            <label for="datetime">Date and time:</label> <br>
+		    <input type="datetime-local" id="datetime" name="datetime" required><br><br>
         </p>
 
+        <p>
+            <label for="file">Upload file:</label> <br>
+			<input type="file" id="file" name="file"><br><br>
+        </p>
 
         <input type="submit" value="Submit"> <br>
         </form>
-    
-
-
-
-
 
         <br> All you get is this text and a mostly barebones HTML document.</p>
   </div>
